63bc095f35f1ebbc36cadb3feb125d5e2de9cb75
jorgemanzo <jmanzo203689@gmail.com>
2020-06-08 23:19:37 -0700

    build should pass now

--- src/compileMe.cpp
+++ src/compileMe.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
+
+int numberOfDigitsInNum(int n)
+{
+  int k = 1;
+  for(int i = 1, j = 9; !(i <= n && n <= j); i*= 10, j*= 10)
+    k++;
+  return k;
+}
 
 char* intToStr(int n)
 {
-    char* a = (char*) malloc(sizeof(char)*20);
+    char* a = (char*) malloc(sizeof(char)*numberOfDigitsInNum(n)+1);
     sprintf(a, "%d", n);
     return a;
 }
 
 int main()
 {
-    char* s = intToStr(2048);
+    char* s = intToStr(128);
     printf("%s\n", s);
 }
